776140de0de4731e4bae80f1765aa82250d1ef63
trongtinIUH <trantin1973@gmail.com>
2025-07-24 23:18:29 +0700

    thêm load dotenv

--- src/main/java/flim/backendcartoon/BackendCartoonApplication.java
+++ src/main/java/flim/backendcartoon/BackendCartoonApplication.java
@@ -1,5 +1,6 @@
 package flim.backendcartoon;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,9 @@ import org.springframework.cache.annotation.EnableCaching;
 public class BackendCartoonApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.load();
+        // Set to System env
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(BackendCartoonApplication.class, args);
     }
 
